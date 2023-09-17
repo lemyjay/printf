bd2252a1b3ced64a8b9a62bdf6d61b502974bfa0
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-09-17 15:03:30 +0000

    TASK 0 - Implementing _printf function that produces output according to a format but for now, with basic %c, %s, and %% format specifiers

---
+++ _printf.c
@@ -0,0 +1,68 @@
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * print_string - A function that prints a string
+ *
+ * @str: a pointer to the string to be printed.
+ * Returns void
+ */
+void print_string(char *str)
+{
+	unsigned int i = 0;
+
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+/**
+ * _printf - A function that produces output according to a format.
+ *
+ * @format: a character string that composes of zero or more directives.
+ *
+ * Return: the number of characters printed (excluding the null byte used to
+ * end the output to strings).
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	unsigned int i = 0;
+
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		if (format[i] == '%')
+		{
+			switch (format[i + 1])
+			{
+				case 'c':
+					write(1, va_arg(args, char), 1);
+					i++;
+					break;
+				case 's':
+					print_string(va_arg(args, char *));
+					i++;
+					break;
+				case '%':
+					write(1, '%', 1);
+					i++;
+					break;
+			}
+		}
+		else
+			write(1, format[i], 1);
+
+		i++;
+	}
+
+	return (i);
+
+
+}
+
