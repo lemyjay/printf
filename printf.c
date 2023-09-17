f1108052f9fa7966870ee19361d717fd46a17217
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-09-17 17:11:01 +0000

    corrections the print.c

--- printf.c
+++ printf.c
@@ -40,7 +40,7 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	while (format[i])
 	{
-		if (format[i] == '%')
+		if (format[i] == '%' && format[i + 1] != '\0')
 		{
 			switch (format[i + 1])
 			{
