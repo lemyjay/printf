b18a02d9444305a41ed61983eaae2345e2b8f0c0
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-09-20 15:45:08 +0000

    corrections

--- handlers0.c
+++ handlers0.c
@@ -19,9 +19,11 @@ va_list args, unsigned int *count, unsigned int *i, int casing, int flags)
 	if (c == '\0')
 		write(1, &c, 1);
 	else
+	{
 		write(1, &c, 1);
-	(*count)++;
-	(*i)++;
+		(*count)++;
+		(*i)++;
+	}
 }
 
 /**
